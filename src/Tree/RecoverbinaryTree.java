6acdce51c6b6fca054e22b423f566cd4b6ef1b05
knanda <kapil.nanda@agoda.com>
2020-03-06 21:17:02 +0700

    fix edge case

--- src/Tree/RecoverbinaryTree.java
+++ src/Tree/RecoverbinaryTree.java
@@ -8,9 +8,10 @@ import java.util.List;
 public class RecoverbinaryTree {
 
     public static void main(String[] args) {
-        TreeNode treeNode = new TreeNode(1);
-        treeNode.left = new TreeNode(3);
-        treeNode.left.right = new TreeNode(2);
+        TreeNode treeNode = new TreeNode(3);
+        treeNode.left = new TreeNode(1);
+        treeNode.right = new TreeNode(4);
+        treeNode.right.left = new TreeNode(2);
 
         recoverTree(treeNode);
         System.out.println(treeNode);
@@ -39,9 +40,10 @@ public class RecoverbinaryTree {
         if (root==null)return;
         if (root.val==i1 || root.val==i2){
             root.val=root.val==i1?i2:i1;
+            if (--count<=0)
+                return;
         }
-        if (--count<=0)
-            return;
+
         fixTree(root.left,count,i1,i2);
         fixTree(root.right,count,i1,i2);
     }
