2e959f448c9a49b9f77b59817129156a7d106200
knanda <kapil.nanda@agoda.com>
2020-03-06 15:22:15 +0700

    refactor merge k list

--- src/Linkedlist/MergeKList.java
+++ src/Linkedlist/MergeKList.java
@@ -32,89 +32,30 @@ public class MergeKList {
     }
 
     public static ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0) return null;
+        if (lists.length == 0) return null;
 
         ListNode result = new ListNode(-1);
-            ListNode prev = result;
-            PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        ListNode prev = result;
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
 
-            for (int i = 0; i < lists.length; i++) {
-                ListNode curr=lists[i];
-                if(curr!=null)
-                    minHeap.add(lists[i]);
-            }
+        for (int i = 0; i < lists.length; i++) {
+            ListNode curr = lists[i];
+            if (curr != null)
+                minHeap.add(lists[i]);
+        }
 
-            System.out.println(minHeap.peek().val);
+        System.out.println(minHeap.peek().val);
 
 
-            while (!minHeap.isEmpty()){
-                ListNode min=minHeap.poll();
-                prev.next=min;
-                prev=prev.next;
-                if (min.next!=null)
+        while (!minHeap.isEmpty()) {
+            ListNode min = minHeap.poll();
+            prev.next = min;
+            prev = prev.next;
+            if (min.next != null)
                 minHeap.add(min.next);
-            }
-
-            return result.next;
-
-    }
-
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-
-        if (l1 == null & l2 == null)
-            return null;
-        else if (l1 == null)
-            return l2;
-        else if (l2 == null)
-            return l1;
-
-        ListNode headL1 = l1;
-        ListNode headL2 = l2;
-        ListNode newHead = null;
-        ListNode pointer = newHead;
-
-        while (headL1 != null && headL2 != null) {
-            ListNode currentL1 = headL1;
-            ListNode currentL2 = headL2;
-
-            if (currentL1.val < currentL2.val) {
-                if (newHead == null) {
-                    newHead = currentL1;
-                    pointer = newHead;
-                } else {
-                    pointer.next = currentL1;
-                    pointer = pointer.next;
-
-                }
-                headL1 = headL1.next;
-            } else {
-                if (newHead == null) {
-                    newHead = currentL2;
-                    pointer = newHead;
-                } else {
-                    pointer.next = currentL2;
-                    pointer = pointer.next;
-                }
-                headL2 = headL2.next;
-            }
         }
 
-        if (headL1 == null) {
-            while (headL2 != null) {
-                pointer.next = headL2;
-                pointer = pointer.next;
-                headL2 = headL2.next;
-            }
-
-        } else {
-            while (headL1 != null) {
-                pointer.next = headL1;
-                pointer = pointer.next;
-                headL1 = headL1.next;
-            }
-        }
-
-        return newHead;
+        return result.next;
     }
 }
 
